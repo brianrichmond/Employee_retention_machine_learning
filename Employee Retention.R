cae2f3678443459e274c8a5c2213e418e03c0b7c
BrianRichmond <brian.richmond@gmail.com>
2018-10-03 17:09:51 -0700

    commit from rstudio

---
+++ Employee Retention.R
@@ -0,0 +1,8 @@
+## Employee Retention Analysis
+## Goals:
+##    1. Create model to accurately predict employees who leave
+##    2. Identify key factors related to employee churn
+## Brian Richmond
+## Updated 2018-10-3
+
+
