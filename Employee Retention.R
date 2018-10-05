44a0b2090ca30f5fd63473bf909352b18bd01231
BrianRichmond <brian.richmond@gmail.com>
2018-10-04 17:19:15 -0700

    commit from rstudio

--- Employee Retention.R
+++ Employee Retention.R
@@ -5,5 +5,24 @@
 ## Brian Richmond
 ## Updated 2018-10-3
 
+
 # load data
+emp <- read.csv("MFG10YearTerminationData_Kaggle.csv", header = TRUE)
+
+# basic EDA
+dim(emp)  # number of rows & columns in data
+str(emp)  # structure of the data, data types
+summary(emp)  # summary stats
+
+## Summary stats show that there are about 7,000 employee ids with records across years from 2006-15
+
+# explore status
+require(tidyr)  # data tidying (e.g., spread)
+require(data.table)  # data table manipulations (e.g., shift)
+status_count <- with(emp, table(STATUS_YEAR, STATUS))
+status_count <- spread(data.frame(status_count), STATUS, Freq)
+status_count$previous_active <- shift(status_count$ACTIVE, 1L, type = "lag")
+status_count$percent_terminated <- 100*status_count$TERMINATED / status_count$previous_active
+status_count
+# status_count <- filter(status_count, !is.na(percent_terminated))
 
