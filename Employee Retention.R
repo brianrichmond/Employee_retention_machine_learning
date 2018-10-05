02da14b08e374ec796ddc2ed64261f08b0dce0ae
BrianRichmond <brian.richmond@gmail.com>
2018-10-05 11:56:52 -0700

    from rstudio, with dplyr added

--- Employee Retention.R
+++ Employee Retention.R
@@ -19,10 +19,12 @@ summary(emp)  # summary stats
 # explore status
 require(tidyr)  # data tidying (e.g., spread)
 require(data.table)  # data table manipulations (e.g., shift)
+require(dplyr)  # data manipulation (e.g., filter)
 status_count <- with(emp, table(STATUS_YEAR, STATUS))
 status_count <- spread(data.frame(status_count), STATUS, Freq)
 status_count$previous_active <- shift(status_count$ACTIVE, 1L, type = "lag")
 status_count$percent_terminated <- 100*status_count$TERMINATED / status_count$previous_active
 status_count
-# status_count <- filter(status_count, !is.na(percent_terminated))
+status_count <- filter(status_count, !is.na(percent_terminated))  # remove first year with NA percent_terminated
+
 
