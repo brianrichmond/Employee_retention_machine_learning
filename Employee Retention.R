319f836abde818a11ce67c684de514242be64db3
BrianRichmond <brian.richmond@gmail.com>
2018-10-05 15:49:08 -0700

    added eda plots

--- Employee Retention.R
+++ Employee Retention.R
@@ -19,12 +19,32 @@ summary(emp)  # summary stats
 # explore status
 require(tidyr)  # data tidying (e.g., spread)
 require(data.table)  # data table manipulations (e.g., shift)
-require(dplyr)  # data manipulation (e.g., filter)
+require(dplyr)  # data manipulation w dataframes (e.g., filter)
 status_count <- with(emp, table(STATUS_YEAR, STATUS))
 status_count <- spread(data.frame(status_count), STATUS, Freq)
 status_count$previous_active <- shift(status_count$ACTIVE, 1L, type = "lag")
 status_count$percent_terminated <- 100*status_count$TERMINATED / status_count$previous_active
 status_count
 status_count <- filter(status_count, !is.na(percent_terminated))  # remove first year with NA percent_terminated
+
+require(ggplot2)
+ggplot() + geom_point(aes(x = STATUS_YEAR, y = percent_terminated), data = status_count) + geom_smooth(method = "lm")  # plot percent_termintaed by year
+
+# create a dataframe of the subset of terminated employees
+terms <- as.data.frame(emp %>% filter(STATUS=="TERMINATED"))
+
+# plot terminations by reason
+ggplot() + geom_bar(aes(y = ..count..,x = STATUS_YEAR, fill = termreason_desc), data=terms, position = position_stack()) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+# plot terminations by reason & department
+ggplot() + geom_bar(aes(y = ..count.., x = department_name, fill = termreason_desc), data=terms, position = position_stack())+
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+
+# plot terms & active by length_of_service and age
+library(caret)
+featurePlot(x=emp[,6:7], y=emp$STATUS,plot="density",auto.key = list(columns = 2))
+featurePlot(x=emp[,6:7], y=emp$STATUS,plot="box",auto.key = list(columns = 2))
+
 
 
