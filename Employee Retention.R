f0e13d74596f3cd275aae09f78a280e9dee24538
BrianRichmond <brian.richmond@gmail.com>
2018-10-05 17:05:20 -0700

    additional plots

--- Employee Retention.R
+++ Employee Retention.R
@@ -17,9 +17,9 @@ summary(emp)  # summary stats
 ## Summary stats show that there are about 7,000 employee ids with records across years from 2006-15
 
 # explore status
-require(tidyr)  # data tidying (e.g., spread)
-require(data.table)  # data table manipulations (e.g., shift)
-require(dplyr)  # data manipulation w dataframes (e.g., filter)
+library(tidyr)  # data tidying (e.g., spread); using 'require' instead of 'library' to avoid repeat library loads
+library(data.table)  # data table manipulations (e.g., shift)
+library(dplyr)  # data manipulation w dataframes (e.g., filter)
 status_count <- with(emp, table(STATUS_YEAR, STATUS))
 status_count <- spread(data.frame(status_count), STATUS, Freq)
 status_count$previous_active <- shift(status_count$ACTIVE, 1L, type = "lag")
@@ -27,7 +27,7 @@ status_count$percent_terminated <- 100*status_count$TERMINATED / status_count$pr
 status_count
 status_count <- filter(status_count, !is.na(percent_terminated))  # remove first year with NA percent_terminated
 
-require(ggplot2)
+library(ggplot2)
 ggplot() + geom_point(aes(x = STATUS_YEAR, y = percent_terminated), data = status_count) + geom_smooth(method = "lm")  # plot percent_termintaed by year
 
 # create a dataframe of the subset of terminated employees
@@ -41,10 +41,20 @@ ggplot() + geom_bar(aes(y = ..count..,x = STATUS_YEAR, fill = termreason_desc), 
 ggplot() + geom_bar(aes(y = ..count.., x = department_name, fill = termreason_desc), data=terms, position = position_stack())+
   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
-# plot terms & active by length_of_service and age
+# plot terminated & active by age & length_of_service
 library(caret)
 featurePlot(x=emp[,6:7], y=emp$STATUS,plot="density",auto.key = list(columns = 2))
 featurePlot(x=emp[,6:7], y=emp$STATUS,plot="box",auto.key = list(columns = 2))
+
+### Modeling
+# Partition the data into training and test sets
+if (!require(rattle)) install.packages('rattle')
+library(rattle) # graphical interface for data science in R
+library(magrittr) # For the %>% and %<>% operators.
+
+set.seed(314) # set a pre-defined value for the random seed so that results are repeatable
+
+#  The data show many more resignations in 2011-14 than in previous years, so this subset will be used as the training set, with the last year (2015) as the test set
 
 
 
