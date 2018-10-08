df67a101a58d241f91861043579e7a2ab6f5cf2f
BrianRichmond <brian.richmond@gmail.com>
2018-10-08 15:30:08 -0700

    more edits

--- Employee Retention.R
+++ Employee Retention.R
@@ -19,7 +19,7 @@ emp$termreason_desc <- as.factor(gsub("Resignaton", "Resignation", emp$termreaso
 ## Summary stats show that there are about 7,000 employee ids with records across years from 2006-15
 
 # explore status
-library(tidyr)  # data tidying (e.g., spread); using 'require' instead of 'library' to avoid repeat library loads
+library(tidyr)  # data tidying (e.g., spread)
 library(data.table)  # data table manipulations (e.g., shift)
 library(dplyr)  # data manipulation w dataframes (e.g., filter)
 status_count <- with(emp, table(STATUS_YEAR, STATUS))
@@ -77,13 +77,25 @@ pROC::roc(emp_term_RF$y, as.numeric(emp_term_RF$predicted))
 ##### ?? plot ROC?
 
 # predictions based on test dataset (2015)
+if(!require(e1071)){install.packages(e1071, dependencies = TRUE)}
 emp_term_RF_pred <- predict(emp_term_RF, newdata = emp_test)
+confusionMatrix(data = emp_term_RF_pred, reference = emp_test$STATUS,
+                mode = "prec_recall")
+
+
+
 emp_term_RF.tbl <- xtabs(~as.numeric(emp_term_RF_pred)+emp_test$STATUS)
 emp_term_RF.tbl
 emp_t_RF.tbl <- prop.table(emp_term_RF.tbl)
 emp_t_RF.tbl
 emp_term_RF.acc <-sum(diag(emp_term_RF.tbl)/sum(emp_term_RF.tbl))
 print(paste("Random Forest Test Accuracy:",emp_term_RF.acc))
+
+
+
+
+
+
 
 # Examine important variables (type 1=mean decrease in accuracy; 2=...in node impurity)
 varImpPlot(emp_term_RF,type=1, main="Variable Importance (Accuracy)",
@@ -100,7 +112,25 @@ emp$resigned <- ifelse(emp$termreason_desc == "Resignation", "Yes", "No")
 emp$resigned <- as.factor(emp$resigned)  # convert to factor (from character)
 summary(emp$resigned)  # see that there are only 385 resignations
 
+res_vars <- c("age","length_of_service","city_name", "department_name","job_title","store_name","gender_full","resigned")
+emp_res_RF <- randomForest(resigned ~ .,
+                            data = emp_train[res_vars],
+                            ntree=500, importance = TRUE,
+                            na.action = na.omit)
+emp_res_RF  # view results & Confusion matrix
 
+# Calculate the AUC (Area Under the Curve) for train set on itself
+library(pROC)
+pROC::roc(emp_res_RF$y, as.numeric(emp_res_RF$predicted))
+
+##### ?? plot ROC?
+
+## edit above to provide 1) Confusion matrix of test dataset,
+##    and 2) accuracy of predicting 'resigned' (RECALL = true positives/(true positives + false negatives))
+
+##### ?? plot ROC?
+
+##### 
 
 ## find an efficient way to plot ROC & Confusion Matrix, esp success of identifying vol_terms
 
