fdb271b796c2b05cddd6cf4ab715e4a9f531ce07
BrianRichmond <brian.richmond@gmail.com>
2018-10-15 18:20:47 -0700

    updates for Markdown prep

--- Employee Retention.R
+++ Employee Retention.R
@@ -61,29 +61,32 @@ ggplot() + geom_bar(aes(y = ..count..,x = STATUS_YEAR, fill = termreason_desc), 
 library(caret) # machine learning package for gbm (generalized boost regression models) + functions to streamline process for predictive models
 featurePlot(x=emp[,6], y=emp$STATUS,plot="density",auto.key = list(columns = 2))
 
+
 ### Modeling
-# Partition the data into training and test sets
+# select variables to be included in model predicting terminations
+term_vars <- c("age","length_of_service","city_name", "department_name","job_title","store_name","gender_full","BUSINESS_UNIT","STATUS")
+
+# import libraries
 library(rattle)  # graphical interface for data science in R
 library(magrittr)  # For %>% and %<>% operators.
-# Here we use all years before 2015 (2006-14) as the training set, with the last year (2015) as the test set
+library(randomForest)  # random forest modeling
+
+# Partition the data into training and test sets
 emp_term_train <- subset(emp, STATUS_YEAR < 2015)
 emp_term_test <- subset(emp, STATUS_YEAR == 2015)
 
-set.seed(314) # set a pre-defined value for the random seed so that results are repeatable
-
-## RANDOM FOREST MODEL of terminations
+## RANDOM FOREST MODEL of Terminations
 ## No NAs in dataset, so no need to impute or take other measures
-library(randomForest)  # random forest modeling
-# select variables to be included in model predicting terminations, resignations (voluntary terminations)
-term_vars <- c("age","length_of_service","city_name", "department_name","job_title","store_name","gender_full","BUSINESS_UNIT","STATUS")
 emp_term_RF <- randomForest(STATUS ~ .,
                             data = emp_term_train[term_vars],
                             ntree=500, importance = TRUE,
                             na.action = na.omit)
 emp_term_RF  # view results & Confusion matrix
 
-# predictions based on test dataset (2015)
+
+## predictions based on test dataset (2015)
 # generate predictions based on test data ("emp_test")
+set.seed(314) # set a pre-defined value for the random seed so that results are repeatable
 emp_term_RF_pred <- predict(emp_term_RF, newdata = emp_term_test)
 if(!"e1071" %in% installed.packages()) install.packages("e1071")  # package e1071 required for confusionMatrix function
 
@@ -101,10 +104,11 @@ var_importance[order(var_importance[,"MeanDecreaseAccuracy"], decreasing = TRUE)
 
 ## 'age' is the most important variable, probably because many of the terminations are retirements
 
+
 ## ----------------
 # Visualize an example of a Decision Tree
 library(rpart.plot)
-set.seed(42)
+set.seed(99)
 # Decision tree model
 rpart_model <- rpart(STATUS ~.,
                      data = emp_term_train[term_vars],
@@ -280,80 +284,3 @@ emp_res_rose_RF_pred_probs <- predict(emp_res_rose_RF, emp_test, type="prob")
 Employees_flight_risk <- as.data.frame(cbind(emp_test$EmployeeID,emp_res_rose_RF_pred_probs, as.character(emp_test$resigned)))
 Employees_flight_risk <- arrange(Employees_flight_risk, desc(Yes))
 head(Employees_flight_risk)
-
-
-
-#################### NOTES ####################
-# Approach 1b
-####################
-## Using gbm library
-####################
-# library(gbm)
-
-##  APPROACH 1
-# from https://rpubs.com/omicsdata/gbm ;
-# also check out https://amunategui.github.io/binary-outcome-modeling/:
-emp_train_b <- emp_train
-emp_train_b$resigned <- as.numeric(emp_train_b$resigned)
-emp_train_b <- transform(emp_train_b, resigned=resigned-1)  # bernoulli distribution requires response to be 0, 1
-emp_res_boost <- gbm(resigned ~ ., data = emp_train_b[res_vars],
-                     distribution = "bernoulli", n.trees = 10000,
-                     shrinkage = 0.01, cv.folds=5, verbose=F)
-# Took 5 min to run w 10000 trees
-emp_res_boost
-best_iter <- gbm.perf(emp_res_boost, method = "cv")
-best_iter
-print(emp_res_boost)  # outputs the key model attributes & best cross-validated iteration
-summary(emp_res_boost)  # summary of variable influences & plot
-## This model identifies the same top 3 main factors as the gbm using the gaussian approach below; not sure that gaussian is the right approach
-# Plot the marginal effect of the selected variables by "integrating" out the other variables.
-plot.gbm(emp_res_boost, 1, best_iter)
-plot.gbm(emp_res_boost, 2, best_iter)
-plot.gbm(emp_res_boost, 3, best_iter)
-
-# also check out https://amunategui.github.io/binary-outcome-modeling/:
-emp_res_boost_preds <- predict(object = emp_res_boost,
-                               emp_test[res_vars],
-                               n.trees = best_iter,
-                               type = "response")
-summary(emp_res_boost_preds)
-
-# Check out model predictions on training set
-#  useful, BUT LACKS CONFUSION MATRIX: http://allstate-university-hackathons.github.io/PredictionChallenge2016/GBM
-emp_res_boost_train_preds <- predict(object = emp_res_boost,
-                                     newdata = emp_train[res_vars],
-                                     n.trees = best_iter,
-                                     type = "response")
-head(emp_res_boost_train_preds)
-head(data.frame("Actual" = emp_train$resigned,
-                "Predicted" = emp_res_boost_train_preds))
-
-##  APPROACH 2
-#  (https://rstudio-pubs-static.s3.amazonaws.com/79417_b67efa7505eb42d7a2986aef215a8b8e.html)
-ctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3, classProbs = TRUE, summaryFunction = twoClassSummary)
-emp_res_gbm <- train(resigned ~ ., data = emp_train[res_vars], method = 'gbm',
-                     trControl = ctrl, metric = 'map', verbose = FALSE)
-### THIS TOOK A LONG TIME
-emp_res_gbm
-summary(emp_res_gbm)
-
-# Predictions of test data set
-emp_res_gbm_pred <- predict(emp_res_gbm, emp_test)
-confusionMatrix(data = emp_res_gbm_pred, reference = emp_test$resigned,
-                positive = "Yes")  # mode = "prec_recall" if preferred
-
-
-#  (https://www.r-bloggers.com/gradient-boosting-in-r/)
-#  NOTES: gaussian may not be the best approach here bc binary category classification
-emp_res_boost_gauss <- gbm(resigned ~ ., data = emp_train[res_vars],
-                           distribution = "gaussian", n.trees = 10000,
-                           shrinkage = 0.01, interaction.depth = 4)
-emp_res_boost_gauss
-summary(emp_res_boost_gauss)
-
-# # plot of 'resigned' with main factors
-# plot(emp_res_boost_gauss, i="age")
-# plot(emp_res_boost_gauss, i="length_of_service")
-# plot(emp_res_boost_gauss, i="city_name")
-
-
